50337e897af9511c7c63780f1150202081f307d9
Lauryn <laurynpearson@gmail.com>
2019-06-28 09:29:56 -0500

    create hello method in prework

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   # get "/photos" => "photos#index"
 
   # EXAMPLE JSON ROUTE WITH API NAMESPACE
-  # namespace :api do
+  namespace :api do
   #   get "/photos" => "photos#index"
-  # end
+  get "/welcomes" => 'welcomes#hello'
+  
+  end
 end
